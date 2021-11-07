0fa2cad567288801811e8128f493577a387b6877
Nikita <artur.rejet.vseh@gmail.com>
2021-11-07 12:50:39 +0200

    Added web plugin realisation

--- lib/flutter_web_plugin_example.dart
+++ lib/flutter_web_plugin_example.dart
@@ -1,13 +1,14 @@
-
 import 'dart:async';
 
 import 'package:flutter/services.dart';
 
 class FlutterWebPluginExample {
-  static const MethodChannel _channel = MethodChannel('flutter_web_plugin_example');
+  static const MethodChannel _channel =
+      MethodChannel('flutter_web_plugin_example');
 
-  static Future<String?> get platformVersion async {
-    final String? version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  static Future<dynamic> sendMethodMessage(String? arguments) async {
+    final dynamic response =
+        await _channel.invokeMethod('sendMethodMessage', arguments);
+    return response;
   }
 }
--- lib/flutter_web_plugin_example_web.dart
+++ lib/flutter_web_plugin_example_web.dart
@@ -1,14 +1,20 @@
-import 'dart:async';
-// In order to *not* need this ignore, consider extracting the "web" version
-// of your plugin as a separate package, instead of inlining it in the same
-// package as the core of your plugin.
-// ignore: avoid_web_libraries_in_flutter
-import 'dart:html' as html show window;
+@JS()
+library callable_function;
 
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:js/js.dart';
+import 'package:js/js_util.dart';
 
-/// A web implementation of the FlutterWebPluginExample plugin.
+@JS('jsOnEvent')
+external set _jsOnEvent(void Function(dynamic event) f);
+
+@JS()
+external dynamic jsInvokeMethod(String method, String? params);
+
 class FlutterWebPluginExampleWeb {
   static void registerWith(Registrar registrar) {
     final MethodChannel channel = MethodChannel(
@@ -19,26 +25,31 @@ class FlutterWebPluginExampleWeb {
 
     final pluginInstance = FlutterWebPluginExampleWeb();
     channel.setMethodCallHandler(pluginInstance.handleMethodCall);
+
+    //Sets the call from JavaScript handler
+    _jsOnEvent = allowInterop((dynamic event) {
+      //Process JavaScript call here
+      debugPrint(event);
+    });
   }
 
-  /// Handles method calls over the MethodChannel of this plugin.
-  /// Note: Check the "federated" architecture for a new way of doing this:
-  /// https://flutter.dev/go/federated-plugins
   Future<dynamic> handleMethodCall(MethodCall call) async {
     switch (call.method) {
-      case 'getPlatformVersion':
-        return getPlatformVersion();
+      case 'sendMethodMessage':
+        return sendMethodMessage(call.method, call.arguments);
       default:
         throw PlatformException(
           code: 'Unimplemented',
-          details: 'flutter_web_plugin_example for web doesn\'t implement \'${call.method}\'',
+          details:
+              'flutter_web_plugin_example for web doesn\'t implement \'${call.method}\'',
         );
     }
   }
 
-  /// Returns a [String] containing the version of the platform.
-  Future<String> getPlatformVersion() {
-    final version = html.window.navigator.userAgent;
-    return Future.value(version);
+  static Future<dynamic> sendMethodMessage(
+      String method, String? arguments) async {
+    final dynamic response =
+        await promiseToFuture(jsInvokeMethod(method, arguments));
+    return response;
   }
 }
